30fdbdf443566fcc246afa66347275ca6a1b46d7
David Lindblom <lindblom.david@hotmail.com>
2026-02-18 13:27:42 +0100

    added basic types

---
+++ teampulse/src/types.ts
@@ -0,0 +1,19 @@
+type Mood = "great" | "good" | "okay" | "tired" | "stressed";
+
+type EnergyLevel = 1 | 2 | 3 | 4 | 5;
+
+interface CheckIn {
+  id: string;
+  name: string;
+  mood: Mood;
+  energy: EnergyLevel;
+  comment?: string;
+  timestamp: Date;
+}
+
+interface DayStats {
+  averageEnergy: number;
+  moodDistrubition: Record<Mood, number>;
+  totalCheckIns: number;
+}
+
